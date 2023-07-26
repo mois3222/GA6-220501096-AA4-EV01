03c45b14893807cc43ec0ec87c29429a3d97e874
mois3222 <moisky222@gmail.com>
2023-07-25 22:53:42 -0500

    add base in vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/GA6-220501096-AA4-EV01/",
+});
